044e7b20d561ad195da14ce9d164e5005371ddb2
SusannaMontero <sumonterog@gmail.com>
2020-12-02 20:10:42 +0100

    2/12/20
    falta reduïr la imatge del logo

--- LaMevaXarxaSocial/src/app/app.component.ts
+++ LaMevaXarxaSocial/src/app/app.component.ts
@@ -7,16 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   title = 'LaMevaXarxaSocial';
-
-
-  // es declara i inicialitza la variable category per assignar a l'html d'aquest component el click sel.leccionat del menú
-  category: string ='All';
-
-
-
-// FUNCIO per canviar d'event al menú segons sel.lecció amb click, entre PERFIL i ADMIN
-
-  canviEvent(category: string): void {
-    this.category = category;
-  }
+ 
 }
