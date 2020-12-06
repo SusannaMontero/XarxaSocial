54b87cfcc3e9f7526cc6c8649abc7a823e4dc611
JosepRodriguez90 <65295788+JosepRodriguez90@users.noreply.github.com>
2020-12-06 22:06:41 +0100

    funciona pero al app.component

--- LaMevaXarxaSocial/src/app/components/admin/admin.component.ts
+++ LaMevaXarxaSocial/src/app/components/admin/admin.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { Usuari } from '../Usuari.llista';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin',
@@ -9,26 +10,37 @@ import { Usuari } from '../Usuari.llista';
 export class AdminComponent implements OnInit {
 
   @Input() usuario: Usuari;
-  deleteEvent: EventEmitter<Usuari> = new EventEmitter<Usuari>();
-  users: Usuari[]=[];
-  userSeleccionat: Usuari = null;
+  @Output() deleteEvent: EventEmitter<Usuari> = new EventEmitter<Usuari>();
 
   constructor() { }
 
   ngOnInit(): void {
-    this.users.push(new Usuari('Josep', 'Rodríguez','99','assets/fotoMozart.jpg','El millor01', 'josep@gmail.com','*****','*****'))
-    this.users.push(new Usuari('Cristian', 'Tortosa','99','assets/manuelCarrasco.jpg','El millor02', 'cristian@gmail.com','****','****'))
-    this.users.push(new Usuari('Ronnie', 'Coleman','99','assets/manuelCarrasco.jpg','El millor05', 'king@gmail.com','**','**'))
-    this.users.push(new Usuari('Aida', 'Jesus','99','assets/fotoMozart.jpg','La millor01', 'aida@gmail.com','*****','*****'))
-    this.users.push(new Usuari('Marc', 'Valdivia','99','assets/manuelCarrasco.jpg','El millor03', 'marc@gmail.com','****','****'))
-    this.users.push(new Usuari('Jordi', 'Obis','99','assets/AntonioOrozco.jpg','El millor04', 'Jordi@gmail.com','**','**'))
   }
 
-  seleccionarUser(users:Usuari):void{
-    this.userSeleccionat = users;
-  }
 
   deleteUsuario(): void {
-    this.deleteEvent.emit(this.usuario);
+
+    Swal.fire({
+      title: 'Estas seguro/a?',
+      text: "Usted no será capaz de revertir esta acción!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.deleteEvent.emit(this.usuario);
+
+        Swal.fire(
+          'Borrado!',
+          'Tu archivo ha sido eliminado.',
+          'success'
+        )
+      }
+    })
+
   }
+
 }
